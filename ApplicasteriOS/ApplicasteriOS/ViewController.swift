18cb3e3493afdcfad757cc7d6ae4da81b0f582ea
George <george@Gios-MBP.home>
2019-10-10 19:06:38 -0400

    Initial commit

---
+++ ApplicasteriOS/ApplicasteriOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicasteriOS
+//
+//  Created by Gio Lomsa on 10/10/19.
+//  Copyright © 2019 Giorgi Lomsadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ApplicasteriOS/ApplicasteriOSTests/ApplicasteriOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  ApplicasteriOSTests.swift
+//  ApplicasteriOSTests
+//
+//  Created by Gio Lomsa on 10/10/19.
+//  Copyright © 2019 Giorgi Lomsadze. All rights reserved.
+//
+
+import XCTest
+@testable import ApplicasteriOS
+
+class ApplicasteriOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
